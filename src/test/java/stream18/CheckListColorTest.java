645caa8b15d6eab071d5baf6c1379d1de12cf8c5
GeronXXX <lityagin.ru@gmail.com>
2023-06-12 14:56:41 +0300

    v. 6

--- src/test/java/stream18/CheckListColorTest.java
+++ src/test/java/stream18/CheckListColorTest.java
@@ -6,108 +6,11 @@ import org.junit.jupiter.api.Test;
 import static helpers.CustomAllureListener.withCustomTemplates;
 import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.hamcrest.Matchers.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static specs.ReqresSpec.reqresRequestSpec;
 import static specs.ReqresSpec.reqresResponseSpec;
 
 public class CheckListColorTest {
-//    @DisplayName("Проверка данных пользователя id, email и статус код")
-//    @Test
-//    void checkSingleUserId() {
-//        SingleUserDataBody response = step("Отправка запроса на получение данных пользователя", () ->
-//                given(reqresRequestSpec)
-//                        .filter(withCustomTemplates())
-//                        .when()
-//                        .get("users/2")
-//                        .then()
-//                        .spec(reqresResponseSpec)
-//                        .statusCode(200)
-//                        .extract().as(SingleUserDataBody.class));
-//
-//        step("Проверка id = 2", () ->
-//                assertEquals(2, response.getUser().getId()));
-//        step("Проверка email = janet.weaver@reqres.in", () ->
-//                assertEquals("janet.weaver@reqres.in", response.getUser().getEmail()));
-//    }
-
-//    @DisplayName("Попытка залогиниться без пароля")
-//    @Test
-//    void checkLoginNotPassword() {
-//        FakerTestDate data = new FakerTestDate();
-//        LoginErrorBody body = new LoginErrorBody();
-//        step("Set email для post запроса", () -> body.setEmail(data.email));
-//
-//        LoginErrorResponse response = step("Отправка post запроса", () ->
-//                given(reqresRequestSpec)
-//                        .filter(withCustomTemplates())
-//                        .body(body)
-//                        .when()
-//                        .post("/login")
-//                        .then()
-//                        .spec(reqresResponseSpec)
-//                        .statusCode(400)
-//                        .extract().as(LoginErrorResponse.class));
-//
-//        step("Проверка ответа Missing password", () ->
-//                assertThat(response.getError()).isEqualTo("Missing password"));
-//    }
-
-//    @DisplayName("Попытка ответа в соответствии со схемой body POJO")
-//    @Test
-//    void checkListResourceScheme() {
-//
-//        step("Отправка get запроса и сверка со схемой ответа POJO", () ->
-//                given(reqresRequestSpec)
-//                        .filter(withCustomTemplates())
-//                        .when()
-//                        .get("/unknown")
-//                        .then()
-//                        .spec(reqresResponseSpec)
-//                        .statusCode(200)
-//                        .body(matchesJsonSchemaInClasspath("schemas/list_resource_schema")));
-//    }
-
-//    @DisplayName("Проверка POST запроса body = response")
-//    @Test
-//    void checkCreate() {
-//        FakerTestDate data = new FakerTestDate();
-//        CreateUserDataBody body = new CreateUserDataBody();
-//        step("Set firstName для post запроса", () -> body.setName(data.firstName));
-//        step("Set job для post запроса", () -> body.setJob(data.jobFaker));
-//
-//        CreateUserDataResponse response = step("Отправка post запроса", () ->
-//                given(reqresRequestSpec)
-//                        .filter(withCustomTemplates())
-//                        .body(body)
-//                        .when()
-//                        .post("/users")
-//                        .then()
-//                        .spec(reqresResponseSpec)
-//                        .statusCode(201)
-//                        .extract().as(CreateUserDataResponse.class));
-//
-//        step("Сверка firstName в body с firstName в response", () ->
-//                assertThat(response.getName()).isEqualTo(data.firstName));
-//        step("Сверка job в body с job в response", () ->
-//                assertThat(response.getJob()).isEqualTo(data.jobFaker));
-//    }
-
-//    @DisplayName("Проверка DELETE запроса")
-//    @Test
-//    void checkDelete() {
-//
-//        step("Проверка статус кода 204 на запрос delete", () ->
-//                given(reqresRequestSpec)
-//                        .filter(withCustomTemplates())
-//                        .when()
-//                        .delete("/users/2")
-//                        .then()
-//                        .spec(reqresResponseSpec)
-//                        .statusCode(204));
-//    }
-
     @DisplayName("Проверка значений цветов используя groovy")
     @Test
     void checkListColor() {
